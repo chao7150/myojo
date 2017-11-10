269e173df43eed87cf8a7780811968e36648fd0c
Satoru Yokoyama <horn051019@yahoo.co.jp>
2017-11-11 05:04:14 +0900

    first commit

---
+++ myojo.py
@@ -0,0 +1,110 @@
+import sys
+import os
+import json
+import twitter
+import glob
+import tweepy
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
+                             QPlainTextEdit, QPushButton, QSizePolicy)
+from PyQt5.QtGui import QIcon
+
+class MyComposer(QPlainTextEdit):
+    def dropEvent(self, event):
+        event.accept()
+        mimeData = event.mimeData()
+        print('dropEvent')
+        for mimetype in mimeData.formats():
+            print('MIMEType:', mimetype)
+            print('Data:', mimeData.data(mimetype))
+            print()
+        print()
+
+class MyWindow(QWidget):
+    '''main window'''
+    
+    def __init__(self):
+        super().__init__()
+        self.accs = {}
+        self.active_accs = []
+
+        self.init_accounts()
+        self.init_window()
+        self.init_widgets()
+        self.show()
+        sys.exit(app.exec_())
+
+    def init_accounts(self):
+        '''load account AT and AS from local and create api object and stream'''
+        if not os.path.exists("images"):
+            os.mkdir("images")
+        if os.path.isfile("auth.json"):
+            with open('auth.json', 'r') as f:
+                authdic = json.load(f)
+            for name, keys in authdic["Twitter"].items():
+                api = twitter.connect(keys["ACCESS_TOKEN"], keys["ACCESS_SECRET"])
+                self.accs[name] = {'api' : api}
+                if not glob.glob("images/" + name + ".*"):
+                    self.accs[name]['icon_path'] = twitter.getmyicon(api, name)
+                else:
+                    self.accs[name]['icon_path'] = glob.glob("images/" + name + ".*")[0]
+        else:
+            default = {
+                "Twitter"  : {},
+                "Mastodon" : {}
+            }
+            with open('auth.json', 'w') as f:
+                json.dump(default, f, indent=2)
+            self.authdic = {}
+
+
+    def init_window(self):
+        #self.setGeometry(300, 100, 200, 125)
+        self.setWindowTitle("myojo")
+
+    def init_widgets(self):
+        self.whole_vbox = QVBoxLayout(self)
+        upper_hbox = QHBoxLayout()
+        middle_hbox = QHBoxLayout()
+        lower_hbox = QHBoxLayout()
+
+        add_account_pushbutton = QPushButton('+')
+        upper_hbox.addWidget(add_account_pushbutton)
+        for key, value in self.accs.items():
+            acc_pushbutton = QPushButton(QIcon(value['icon_path']), None, None)
+            acc_pushbutton.setWhatsThis(key)
+            acc_pushbutton.setCheckable(True)
+            acc_pushbutton.toggled.connect(self.choose_account)
+            upper_hbox.addWidget(acc_pushbutton)
+
+        self.compose_textedit = MyComposer()
+        middle_hbox.addWidget(self.compose_textedit)
+        submit_pushbutton = QPushButton('submit')
+        submit_pushbutton.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Ignored)
+        submit_pushbutton.clicked.connect(self.submit)
+        middle_hbox.addWidget(submit_pushbutton)
+
+        self.whole_vbox.addLayout(upper_hbox)
+        self.whole_vbox.addLayout(middle_hbox)
+        self.whole_vbox.addLayout(lower_hbox)
+
+    def submit(self):
+        if not self.active_accs:
+            return
+        submittext = self.compose_textedit.toPlainText()
+        if not submittext:
+            return
+        for key in self.active_accs:
+            self.accs[key]['api'].update_status(submittext)
+        self.compose_textedit.setPlainText("")
+    
+    def choose_account(self):
+        acc = self.sender()
+        if acc.isChecked():
+            self.active_accs.append(acc.whatsThis())
+        else:
+            self.active_accs.remove(acc.whatsThis())        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mywindow = MyWindow()
+    
---
+++ twitter.py
@@ -0,0 +1,126 @@
+#from PyQt5 import QtCore
+import profile
+import webbrowser
+import json
+import tweepy
+import requests
+import re
+from PyQt5.QtWidgets import QInputDialog
+
+class StreamListener(tweepy.StreamListener):
+    """receive streaming and classify messages"""
+    def __init__(self, name, callback, RTfilter, icon):
+        super(StreamListener, self).__init__()
+        self.callback = callback
+        self.name = name
+        self.RTfilter = RTfilter
+        self.icon = icon
+
+    def on_status(self, status):
+        """send tweets to main gui"""
+        if self.RTfilter and status.text[0:2] == "RT":
+            return
+        else:
+            self.callback(status, self.name, self.icon)
+    
+    def on_event(self, status):
+        print(status)
+        self.callback(status, self.name, self.icon)
+
+    def on_error(self, status_code):
+        """print error code when receive error"""
+        print(status_code)
+        if status_code == 420:
+            print(str(status_code))
+            return False
+
+def open_userstream(api, callback, name):
+    """
+    create and start stream with async mode which enables parallel
+    processing of gui event loop and stream wait loop
+    """
+    stream = tweepy.Stream(auth=api.auth, listener=StreamListener(name, callback, False, True))
+    stream.userstream(async=True)
+    return stream
+
+def open_filterstream(api, callback, name, query):
+    stream = tweepy.Stream(auth=api.auth, listener=StreamListener(name, callback, True, False))
+    stream.filter(track=query, async=True)
+    return stream
+
+def connect(accesstoken, accesssecret):
+    """create api object from saved keys"""
+    auth = tweepy.OAuthHandler(profile.CONSUMER_KEY, profile.CONSUMER_SECRET)
+    auth.set_access_token(accesstoken, accesssecret)
+    api = tweepy.API(auth)
+    return api
+
+def authentication():
+    """register new account and save AT and AS to local file"""
+    consumerkey = profile.CONSUMER_KEY
+    consumersecret = profile.CONSUMER_SECRET
+    auth = tweepy.OAuthHandler(consumerkey, consumersecret)
+
+    redirect_url = auth.get_authorization_url()
+    webbrowser.open(redirect_url)
+
+    verifier, ok = QInputDialog.getText(None, 'PIN code', 'input pin code') # PINコード入力用ダイアログ
+    if not verifier:
+        return
+    verifier = verifier.strip()
+
+    auth.get_access_token(verifier)
+    accesstoken = auth.access_token
+    accesssecret = auth.access_token_secret
+
+    auth.set_access_token(accesstoken, accesssecret)
+    api = tweepy.API(auth)
+    screen_name = api.me().screen_name
+
+    with open('auth.json', 'r') as f:
+        authkeys = json.load(f)
+    if not screen_name in authkeys["Twitter"]:
+        authkeys["Twitter"][screen_name] = {"ACCESS_TOKEN":accesstoken, "ACCESS_SECRET":accesssecret}
+    with open('auth.json', 'w')as f:
+        json.dump(authkeys, f, indent=2)
+    return api, screen_name
+
+def getmyicon(api, screen_name):
+    """get icon image with api and screen_name and save it to local"""
+    url = api.me().profile_image_url_https
+    return geticon(url, screen_name)
+
+def geticon(url, screen_name):
+    response = requests.get(url, allow_redirects=False)
+    image = response.content
+    ext = which_ext(image)
+    imagename = 'images/' + screen_name + ext
+    with open(imagename, 'wb') as f:
+        f.write(image)
+    return imagename
+
+def which_ext(image):
+    '''check image format
+    thanks to http://xaro.hatenablog.jp/entry/2017/05/17/103000'''
+    if re.match(b"^\xff\xd8", image[:2]):
+        return ".jpg"
+    elif re.match(b"^\x89\x50\x4e\x47\x0d\x0a\x1a\x0a", image[:8]):
+        return ".png"
+    elif re.match(b"^\x47\x49\x46\x38", image[:4]):
+        return ".gif"
+    elif re.match(b"^\x42\x4d", image[:2]):
+        return ".bmp"
+    else:
+        print(image)
+    
+def get_allimages(api, id):
+    status = api.get_status(id)
+    images = []
+    for i in status.extended_entities["media"]:
+        url = i["media_url_https"]
+        response = requests.get(url, allow_redirects=False)
+        images.append(response.content)
+    return images
+
+if __name__ == "__main__":
+    authentication()
